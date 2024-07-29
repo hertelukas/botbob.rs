85399c722af27ef75cb54aa63f599cf2891d3f7c
Lukas <lukas.hertel@tum.de>
2024-07-29 19:32:34 +0200

    Use BigInts instead of integers for i64

--- migrations/2024-07-12-144642_create_users/up.sql
+++ migrations/2024-07-12-144642_create_users/up.sql
@@ -1,6 +1,6 @@
 -- Your SQL goes here
 CREATE TABLE users (
-    id INTEGER PRIMARY KEY NOT NULL,
-    points INTEGER NOT NULL,
+    id BIGINT PRIMARY KEY NOT NULL,
+    points BIGINT NOT NULL,
     username TEXT NOT NULL
 )
